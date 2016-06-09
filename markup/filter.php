27d9d7c792425a6a06c2ede587c281e30dc021f9
Bogoslavskiy valentyn <valentyn.bogoslavskiy@lemberg.co.uk>
2016-06-09 18:57:55 +0300

    Init project markup.

---
+++ markup/filter.php
@@ -0,0 +1,228 @@
+<section class="filter-nav">
+    <nav class="filter-nav_wrap">
+        <ul>
+            <li>
+                <label class="checkbox-view">
+                    <strong class="check-all">Filter</strong>
+                    <input type="checkbox">
+                    <svg version="1.1" class="checkbox" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+                         width="20px" height="20px" viewBox="0.667 -0.333 20 20" enable-background="new 0.667 -0.333 20 20" xml:space="preserve">
+                    <path class="check" d="M16.889,1.667H4.445c-0.986,0-1.778,0.8-1.778,1.778V15.89c0,0.977,0.792,1.777,1.778,1.777H16.89
+                        c0.985,0,1.777-0.8,1.777-1.777V3.445C18.666,2.467,17.875,1.667,16.889,1.667z M8.889,14.111L4.445,9.667l1.253-1.253l3.191,3.182
+                        l6.747-6.747l1.253,1.263L8.889,14.111z"/>
+                        <path class="contour" d="M16.889,3.445V15.89H4.445V3.445H16.889 M16.889,1.667H4.445c-0.978,0-1.778,0.8-1.778,1.778V15.89
+                        c0,0.978,0.8,1.777,1.778,1.777H16.89c0.977,0,1.777-0.8,1.777-1.777V3.445C18.666,2.467,17.866,1.667,16.889,1.667z"/>
+                    </svg>
+                </label>
+            </li>
+        </ul>
+        <ul>
+            <li>
+                <strong>Experience Levels</strong>
+            </li>
+            <li>
+                <label class="checkbox-view">
+                    Begginer
+                    <input type="checkbox">
+                    <svg version="1.1" class="checkbox" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+                         width="20px" height="20px" viewBox="0.667 -0.333 20 20" enable-background="new 0.667 -0.333 20 20" xml:space="preserve">
+                    <path class="check" d="M16.889,1.667H4.445c-0.986,0-1.778,0.8-1.778,1.778V15.89c0,0.977,0.792,1.777,1.778,1.777H16.89
+                        c0.985,0,1.777-0.8,1.777-1.777V3.445C18.666,2.467,17.875,1.667,16.889,1.667z M8.889,14.111L4.445,9.667l1.253-1.253l3.191,3.182
+                        l6.747-6.747l1.253,1.263L8.889,14.111z"/>
+                        <path class="contour" d="M16.889,3.445V15.89H4.445V3.445H16.889 M16.889,1.667H4.445c-0.978,0-1.778,0.8-1.778,1.778V15.89
+                        c0,0.978,0.8,1.777,1.778,1.777H16.89c0.977,0,1.777-0.8,1.777-1.777V3.445C18.666,2.467,17.866,1.667,16.889,1.667z"/>
+                    </svg>
+                </label>
+            </li>
+            <li>
+                <label class="checkbox-view">
+                    Intermediate
+                    <input type="checkbox">
+                    <svg version="1.1" class="checkbox" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+                         width="20px" height="20px" viewBox="0.667 -0.333 20 20" enable-background="new 0.667 -0.333 20 20" xml:space="preserve">
+                    <path class="check" d="M16.889,1.667H4.445c-0.986,0-1.778,0.8-1.778,1.778V15.89c0,0.977,0.792,1.777,1.778,1.777H16.89
+                        c0.985,0,1.777-0.8,1.777-1.777V3.445C18.666,2.467,17.875,1.667,16.889,1.667z M8.889,14.111L4.445,9.667l1.253-1.253l3.191,3.182
+                        l6.747-6.747l1.253,1.263L8.889,14.111z"/>
+                        <path class="contour" d="M16.889,3.445V15.89H4.445V3.445H16.889 M16.889,1.667H4.445c-0.978,0-1.778,0.8-1.778,1.778V15.89
+                        c0,0.978,0.8,1.777,1.778,1.777H16.89c0.977,0,1.777-0.8,1.777-1.777V3.445C18.666,2.467,17.866,1.667,16.889,1.667z"/>
+                    </svg>
+                </label>
+            </li>
+            <li>
+                <label class="checkbox-view">
+                    Advanced
+                    <input type="checkbox">
+                    <svg version="1.1" class="checkbox" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+                         width="20px" height="20px" viewBox="0.667 -0.333 20 20" enable-background="new 0.667 -0.333 20 20" xml:space="preserve">
+                    <path class="check" d="M16.889,1.667H4.445c-0.986,0-1.778,0.8-1.778,1.778V15.89c0,0.977,0.792,1.777,1.778,1.777H16.89
+                        c0.985,0,1.777-0.8,1.777-1.777V3.445C18.666,2.467,17.875,1.667,16.889,1.667z M8.889,14.111L4.445,9.667l1.253-1.253l3.191,3.182
+                        l6.747-6.747l1.253,1.263L8.889,14.111z"/>
+                        <path class="contour" d="M16.889,3.445V15.89H4.445V3.445H16.889 M16.889,1.667H4.445c-0.978,0-1.778,0.8-1.778,1.778V15.89
+                        c0,0.978,0.8,1.777,1.778,1.777H16.89c0.977,0,1.777-0.8,1.777-1.777V3.445C18.666,2.467,17.866,1.667,16.889,1.667z"/>
+                    </svg>
+                </label>
+            </li>
+
+        </ul>
+        <ul>
+            <li>
+                <strong>Tracks</strong>
+            </li>
+            <li>
+                <label class="checkbox-view">
+                    Business
+                    <input type="checkbox">
+                    <svg version="1.1" class="checkbox" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+                         width="20px" height="20px" viewBox="0.667 -0.333 20 20" enable-background="new 0.667 -0.333 20 20" xml:space="preserve">
+                    <path class="check" d="M16.889,1.667H4.445c-0.986,0-1.778,0.8-1.778,1.778V15.89c0,0.977,0.792,1.777,1.778,1.777H16.89
+                        c0.985,0,1.777-0.8,1.777-1.777V3.445C18.666,2.467,17.875,1.667,16.889,1.667z M8.889,14.111L4.445,9.667l1.253-1.253l3.191,3.182
+                        l6.747-6.747l1.253,1.263L8.889,14.111z"/>
+                        <path class="contour" d="M16.889,3.445V15.89H4.445V3.445H16.889 M16.889,1.667H4.445c-0.978,0-1.778,0.8-1.778,1.778V15.89
+                        c0,0.978,0.8,1.777,1.778,1.777H16.89c0.977,0,1.777-0.8,1.777-1.777V3.445C18.666,2.467,17.866,1.667,16.889,1.667z"/>
+                    </svg>
+                </label>
+            </li>
+            <li>
+                <label class="checkbox-view">
+                    Coding and Development
+                    <input type="checkbox">
+                    <svg version="1.1" class="checkbox" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+                         width="20px" height="20px" viewBox="0.667 -0.333 20 20" enable-background="new 0.667 -0.333 20 20" xml:space="preserve">
+                    <path class="check" d="M16.889,1.667H4.445c-0.986,0-1.778,0.8-1.778,1.778V15.89c0,0.977,0.792,1.777,1.778,1.777H16.89
+                        c0.985,0,1.777-0.8,1.777-1.777V3.445C18.666,2.467,17.875,1.667,16.889,1.667z M8.889,14.111L4.445,9.667l1.253-1.253l3.191,3.182
+                        l6.747-6.747l1.253,1.263L8.889,14.111z"/>
+                        <path class="contour" d="M16.889,3.445V15.89H4.445V3.445H16.889 M16.889,1.667H4.445c-0.978,0-1.778,0.8-1.778,1.778V15.89
+                        c0,0.978,0.8,1.777,1.778,1.777H16.89c0.977,0,1.777-0.8,1.777-1.777V3.445C18.666,2.467,17.866,1.667,16.889,1.667z"/>
+                    </svg>
+                </label>
+            </li>
+            <li>
+                <label class="checkbox-view">
+                    Community Keynote
+                    <input type="checkbox">
+                    <svg version="1.1" class="checkbox" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+                         width="20px" height="20px" viewBox="0.667 -0.333 20 20" enable-background="new 0.667 -0.333 20 20" xml:space="preserve">
+                    <path class="check" d="M16.889,1.667H4.445c-0.986,0-1.778,0.8-1.778,1.778V15.89c0,0.977,0.792,1.777,1.778,1.777H16.89
+                        c0.985,0,1.777-0.8,1.777-1.777V3.445C18.666,2.467,17.875,1.667,16.889,1.667z M8.889,14.111L4.445,9.667l1.253-1.253l3.191,3.182
+                        l6.747-6.747l1.253,1.263L8.889,14.111z"/>
+                        <path class="contour" d="M16.889,3.445V15.89H4.445V3.445H16.889 M16.889,1.667H4.445c-0.978,0-1.778,0.8-1.778,1.778V15.89
+                        c0,0.978,0.8,1.777,1.778,1.777H16.89c0.977,0,1.777-0.8,1.777-1.777V3.445C18.666,2.467,17.866,1.667,16.889,1.667z"/>
+                    </svg>
+                </label>
+            </li>
+            <li>
+                <label class="checkbox-view">
+                    Drupal.ord
+                    <input type="checkbox">
+                    <svg version="1.1" class="checkbox" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+                         width="20px" height="20px" viewBox="0.667 -0.333 20 20" enable-background="new 0.667 -0.333 20 20" xml:space="preserve">
+                    <path class="check" d="M16.889,1.667H4.445c-0.986,0-1.778,0.8-1.778,1.778V15.89c0,0.977,0.792,1.777,1.778,1.777H16.89
+                        c0.985,0,1.777-0.8,1.777-1.777V3.445C18.666,2.467,17.875,1.667,16.889,1.667z M8.889,14.111L4.445,9.667l1.253-1.253l3.191,3.182
+                        l6.747-6.747l1.253,1.263L8.889,14.111z"/>
+                        <path class="contour" d="M16.889,3.445V15.89H4.445V3.445H16.889 M16.889,1.667H4.445c-0.978,0-1.778,0.8-1.778,1.778V15.89
+                        c0,0.978,0.8,1.777,1.778,1.777H16.89c0.977,0,1.777-0.8,1.777-1.777V3.445C18.666,2.467,17.866,1.667,16.889,1.667z"/>
+                    </svg>
+                </label>
+            </li>
+            <li>
+                <label class="checkbox-view">
+                    Front End
+                    <input type="checkbox">
+                    <svg version="1.1" class="checkbox" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+                         width="20px" height="20px" viewBox="0.667 -0.333 20 20" enable-background="new 0.667 -0.333 20 20" xml:space="preserve">
+                    <path class="check" d="M16.889,1.667H4.445c-0.986,0-1.778,0.8-1.778,1.778V15.89c0,0.977,0.792,1.777,1.778,1.777H16.89
+                        c0.985,0,1.777-0.8,1.777-1.777V3.445C18.666,2.467,17.875,1.667,16.889,1.667z M8.889,14.111L4.445,9.667l1.253-1.253l3.191,3.182
+                        l6.747-6.747l1.253,1.263L8.889,14.111z"/>
+                        <path class="contour" d="M16.889,3.445V15.89H4.445V3.445H16.889 M16.889,1.667H4.445c-0.978,0-1.778,0.8-1.778,1.778V15.89
+                        c0,0.978,0.8,1.777,1.778,1.777H16.89c0.977,0,1.777-0.8,1.777-1.777V3.445C18.666,2.467,17.866,1.667,16.889,1.667z"/>
+                    </svg>
+                </label>
+            </li>
+            <li>
+                <label class="checkbox-view">
+                    Horizons
+                    <input type="checkbox">
+                    <svg version="1.1" class="checkbox" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+                         width="20px" height="20px" viewBox="0.667 -0.333 20 20" enable-background="new 0.667 -0.333 20 20" xml:space="preserve">
+                    <path class="check" d="M16.889,1.667H4.445c-0.986,0-1.778,0.8-1.778,1.778V15.89c0,0.977,0.792,1.777,1.778,1.777H16.89
+                        c0.985,0,1.777-0.8,1.777-1.777V3.445C18.666,2.467,17.875,1.667,16.889,1.667z M8.889,14.111L4.445,9.667l1.253-1.253l3.191,3.182
+                        l6.747-6.747l1.253,1.263L8.889,14.111z"/>
+                        <path class="contour" d="M16.889,3.445V15.89H4.445V3.445H16.889 M16.889,1.667H4.445c-0.978,0-1.778,0.8-1.778,1.778V15.89
+                        c0,0.978,0.8,1.777,1.778,1.777H16.89c0.977,0,1.777-0.8,1.777-1.777V3.445C18.666,2.467,17.866,1.667,16.889,1.667z"/>
+                    </svg>
+                </label>
+            </li>
+            <li>
+                <label class="checkbox-view">
+                    PHP
+                    <input type="checkbox">
+                    <svg version="1.1" class="checkbox" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+                         width="20px" height="20px" viewBox="0.667 -0.333 20 20" enable-background="new 0.667 -0.333 20 20" xml:space="preserve">
+                    <path class="check" d="M16.889,1.667H4.445c-0.986,0-1.778,0.8-1.778,1.778V15.89c0,0.977,0.792,1.777,1.778,1.777H16.89
+                        c0.985,0,1.777-0.8,1.777-1.777V3.445C18.666,2.467,17.875,1.667,16.889,1.667z M8.889,14.111L4.445,9.667l1.253-1.253l3.191,3.182
+                        l6.747-6.747l1.253,1.263L8.889,14.111z"/>
+                        <path class="contour" d="M16.889,3.445V15.89H4.445V3.445H16.889 M16.889,1.667H4.445c-0.978,0-1.778,0.8-1.778,1.778V15.89
+                        c0,0.978,0.8,1.777,1.778,1.777H16.89c0.977,0,1.777-0.8,1.777-1.777V3.445C18.666,2.467,17.866,1.667,16.889,1.667z"/>
+                    </svg>
+                </label>
+            </li>
+            <li>
+                <label class="checkbox-view">
+                    Project Management
+                    <input type="checkbox">
+                    <svg version="1.1" class="checkbox" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+                         width="20px" height="20px" viewBox="0.667 -0.333 20 20" enable-background="new 0.667 -0.333 20 20" xml:space="preserve">
+                    <path class="check" d="M16.889,1.667H4.445c-0.986,0-1.778,0.8-1.778,1.778V15.89c0,0.977,0.792,1.777,1.778,1.777H16.89
+                        c0.985,0,1.777-0.8,1.777-1.777V3.445C18.666,2.467,17.875,1.667,16.889,1.667z M8.889,14.111L4.445,9.667l1.253-1.253l3.191,3.182
+                        l6.747-6.747l1.253,1.263L8.889,14.111z"/>
+                        <path class="contour" d="M16.889,3.445V15.89H4.445V3.445H16.889 M16.889,1.667H4.445c-0.978,0-1.778,0.8-1.778,1.778V15.89
+                        c0,0.978,0.8,1.777,1.778,1.777H16.89c0.977,0,1.777-0.8,1.777-1.777V3.445C18.666,2.467,17.866,1.667,16.889,1.667z"/>
+                    </svg>
+                </label>
+            </li>
+            <li>
+                <label class="checkbox-view">
+                    Site Building
+                    <input type="checkbox">
+                    <svg version="1.1" class="checkbox" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+                         width="20px" height="20px" viewBox="0.667 -0.333 20 20" enable-background="new 0.667 -0.333 20 20" xml:space="preserve">
+                    <path class="check" d="M16.889,1.667H4.445c-0.986,0-1.778,0.8-1.778,1.778V15.89c0,0.977,0.792,1.777,1.778,1.777H16.89
+                        c0.985,0,1.777-0.8,1.777-1.777V3.445C18.666,2.467,17.875,1.667,16.889,1.667z M8.889,14.111L4.445,9.667l1.253-1.253l3.191,3.182
+                        l6.747-6.747l1.253,1.263L8.889,14.111z"/>
+                        <path class="contour" d="M16.889,3.445V15.89H4.445V3.445H16.889 M16.889,1.667H4.445c-0.978,0-1.778,0.8-1.778,1.778V15.89
+                        c0,0.978,0.8,1.777,1.778,1.777H16.89c0.977,0,1.777-0.8,1.777-1.777V3.445C18.666,2.467,17.866,1.667,16.889,1.667z"/>
+                    </svg>
+                </label>
+            </li>
+            <li>
+                <label class="checkbox-view">
+                    Symfony
+                    <input type="checkbox">
+                    <svg version="1.1" class="checkbox" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+                         width="20px" height="20px" viewBox="0.667 -0.333 20 20" enable-background="new 0.667 -0.333 20 20" xml:space="preserve">
+                    <path class="check" d="M16.889,1.667H4.445c-0.986,0-1.778,0.8-1.778,1.778V15.89c0,0.977,0.792,1.777,1.778,1.777H16.89
+                        c0.985,0,1.777-0.8,1.777-1.777V3.445C18.666,2.467,17.875,1.667,16.889,1.667z M8.889,14.111L4.445,9.667l1.253-1.253l3.191,3.182
+                        l6.747-6.747l1.253,1.263L8.889,14.111z"/>
+                        <path class="contour" d="M16.889,3.445V15.89H4.445V3.445H16.889 M16.889,1.667H4.445c-0.978,0-1.778,0.8-1.778,1.778V15.89
+                        c0,0.978,0.8,1.777,1.778,1.777H16.89c0.977,0,1.777-0.8,1.777-1.777V3.445C18.666,2.467,17.866,1.667,16.889,1.667z"/>
+                    </svg>
+                </label>
+            </li>
+            <li>
+                <label class="checkbox-view">
+                    UX
+                    <input type="checkbox">
+                    <svg version="1.1" class="checkbox" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+                         width="20px" height="20px" viewBox="0.667 -0.333 20 20" enable-background="new 0.667 -0.333 20 20" xml:space="preserve">
+                    <path class="check" d="M16.889,1.667H4.445c-0.986,0-1.778,0.8-1.778,1.778V15.89c0,0.977,0.792,1.777,1.778,1.777H16.89
+                        c0.985,0,1.777-0.8,1.777-1.777V3.445C18.666,2.467,17.875,1.667,16.889,1.667z M8.889,14.111L4.445,9.667l1.253-1.253l3.191,3.182
+                        l6.747-6.747l1.253,1.263L8.889,14.111z"/>
+                        <path class="contour" d="M16.889,3.445V15.89H4.445V3.445H16.889 M16.889,1.667H4.445c-0.978,0-1.778,0.8-1.778,1.778V15.89
+                        c0,0.978,0.8,1.777,1.778,1.777H16.89c0.977,0,1.777-0.8,1.777-1.777V3.445C18.666,2.467,17.866,1.667,16.889,1.667z"/>
+                    </svg>
+                </label>
+            </li>
+
+        </ul>
+    </nav>
+</section>
---
+++ markup/info.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <?php include_once("head.php");?>
+  </head>
+  <body class="open">
+  <div class="load">
+    <img src="images/default.gif" width="40" alt="">
+  </div>
+    <header>
+      <div class="top-menu">
+        <div class="hamburger-box">
+              <span class="hamburger">
+              </span>
+        </div>
+        <span class="info-text">Info</span>
+      </div>
+    </header>
+    <div id="page">
+      <div class="inner cf">
+          <?php include_once("left-sidebar.php");?>
+          <section class="info">
+            <ul class="nav-info">
+              <li class="active"><a href="#">About DrupalCon</a></li>
+              <li><a href="#">About this app</a></li>
+            </ul>
+          </section>
+          <section class="description">
+            <p>
+              Lorem ipsum dolor sit amet, vulputate volutpat leo in vehicula vitae aenean. Leo pede molestie a. Vitae optio litora aut, quisque id, vel proin urna corrupti, eu rhoncus et massa in. Quam enim, mi dui ac cras habitasse sem volutpat, a ut, lobortis mattis nam quam aliquam quam. Aliquam hendrerit suspendisse
+              Lorem ipsum dolor sit amet, vulputate volutpat leo in vehicula vitae aenean. Leo pede molestie a. Vitae optio litora aut, quisque id, vel proin urna corrupti, eu rhoncus et massa in. Quam enim, mi dui ac cras habitasse sem volutpat, a ut, lobortis mattis nam quam aliquam quam. Aliquam hendrerit suspendisse
+              Lorem ipsum dolor sit amet, vulputate volutpat leo in vehicula vitae aenean. Leo pede molestie a. Vitae optio litora aut, quisque id, vel proin urna corrupti, eu rhoncus et massa in. Quam enim, mi dui ac cras habitasse sem volutpat, a ut, lobortis mattis nam quam aliquam quam. Aliquam hendrerit suspendisse
+              Lorem ipsum dolor sit amet, vulputate volutpat leo in vehicula vitae aenean. Leo pede molestie a. Vitae optio litora aut, quisque id, vel proin urna corrupti, eu rhoncus et massa in. Quam enim, mi dui ac cras habitasse sem volutpat, a ut, lobortis mattis nam quam aliquam quam. Aliquam hendrerit suspendisse
+              Lorem ipsum dolor sit amet, vulputate volutpat leo in vehicula vitae aenean. Leo pede molestie a. Vitae optio litora aut, quisque id, vel proin urna corrupti, eu rhoncus et massa in. Quam enim, mi dui ac cras habitasse sem volutpat, a ut, lobortis mattis nam quam aliquam quam. Aliquam hendrerit suspendisse
+            </p>
+          </section>
+        </div>
+    </div>
+    <div class="over"></div>
+  </body>
+</html>
