55ed01eddcf5d7f12d9dca486f88e0a6855fc2c3
Yuriy Myrosh <yuriy.myrosh@lemberg.co.uk>
2016-06-10 17:43:49 +0300

    Spikers Search

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,23 +13,34 @@ function filter(){
         $(this).toggleClass('active')
     });
 }
+function search(){
+    $(document).on('click', '.header .close', function(){
+        $(this).parents('.header').removeClass('active');
+    });
+    $(document).on('click', '.header .icon-search', function(){
+        $(this).parents('.header').toggleClass('active');
+    });
 
-$(document).on('click','.hamburger-box',function(e){
-    e.preventDefault();
-    e.stopPropagation();
-    $('body').toggleClass('open');
-});
-
-$(document).on('click', '.over',function(e){
-    e.preventDefault();
-    e.stopPropagation();
-    $('body').removeClass('open');
-});
-
+}
 $(document).ready(function() {
-
+    $(document).on('click', '.hamburger-box',function(e){
+        e.preventDefault();
+        e.stopPropagation();
+        $('body').toggleClass('open');
+    });
+    $(document).on('click', '.over',function(e){
+        e.preventDefault();
+        e.stopPropagation();
+        $('body').removeClass('open');
+    });
     cls();
     filter();
+    search();
+    $(window).load(function() {
+        setTimeout(function() {
+            $('.load').remove('.load');
+        }, 600);
+    });
     $(window).resize(function(){
         cls();
     });
