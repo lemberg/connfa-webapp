8f8eea003c7dcbe2a76be01aa51832af68a6d43f
Yuriy Myrosh <yuriy.myrosh@lemberg.co.uk>
2016-06-13 19:24:00 +0300

    gulp styles compiler fix

--- gulpfile.js
+++ gulpfile.js
@@ -71,7 +71,7 @@ gulp.task('build-fonts', function () {
 
 gulp.task('watch', function () {
     gulp.watch(appDev + '**/*.ts', ['build-ts']);
-    gulp.watch(assetsDev + 'scss/**/*.scss', ['build-css']);
+    gulp.watch(assetsDev + 'styles/**/*.scss', ['build-css']);
     gulp.watch(assetsDev + 'images/*', ['build-img']);
     gulp.watch(assetsDev + 'js/*', ['build-js']);
     gulp.watch(assetsDev + 'fonts/*', ['build-fonts']);
