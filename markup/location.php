b5a11e54a34a83fc05f587e18e5b6574a3b53d02
Bogoslavskiy valentyn <valentyn.bogoslavskiy@lemberg.co.uk>
2016-06-14 13:34:55 +0300

    Add page location and added style for response block iframe.

---
+++ markup/location.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <?php include_once("head.php");?>
+</head>
+<body class="open">
+<div class="load">
+    <img src="images/default.gif" width="40" alt="">
+</div>
+<header>
+    <div class="top-menu">
+        <div class="hamburger-box">
+              <span class="hamburger">
+              </span>
+        </div>
+        <span class="info-text">Location</span>
+    </div>
+</header>
+<div id="page">
+    <div class="inner cf">
+        <?php include_once("left-sidebar.php");?>
+        <div class="location">
+            <div class="box">
+                <h1>New Orleans Ernest N/ Morial Convention Center - H</h1>
+                <p>900 Convention Center Blvd, New Orleans, LA 70130</p>
+            </div>
+            <div class="response">
+                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d4889.530554445859!2d-90.06654943471534!3d29.938589480723603!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8620a66ff3d63175%3A0x77a4ee087282bbaf!2sHampton+Inn+%26+Suites+New+Orleans+Convention+Center!5e0!3m2!1sen!2sua!4v1465897114216" width="100%" height="100%" frameborder="0" style="border:0" allowfullscreen></iframe>
+            </div>
+        </div>
+    </div>
+</div>
+<div class="over"></div>
+</body>
+</html>
+
+
+
