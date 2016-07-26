7fd6c7ad4303b39287c144130f16f75504671b7a
Yuriy Myrosh <yuriy.myrosh@lemberg.co.uk>
2016-07-26 12:37:15 +0300

    fix: floor plans order issue

--- dev/services/floor.service.ts
+++ dev/services/floor.service.ts
@@ -11,6 +11,25 @@ export class FloorService {
     floors:Floor[];
 
     getFloors() {
-        return this._apiService.getCollection('floors');
+        if (!this.floors || !this.floors.length) {
+
+            return new Promise((resolve, reject) => {
+                this._apiService.getCollection('floors').then((floors: Floor[]) => {
+                    floors.sort((a, b) => {
+                        if (a.order < b.order) {
+                            return -1;
+                        } else if (a.order > b.order) {
+                            return 1;
+                        } else {
+                            return 0;
+                        }
+                    });
+                    this.floors = floors;
+                    resolve(floors);
+                })
+            });
+        } else {
+            return Promise.resolve(this.floors);
+        }
     }
 }
