1573560c2a465f9eaaea8e81e8467469daaee64d
Bogoslavskiy valentyn <valentyn.bogoslavskiy@lemberg.co.uk>
2017-04-10 15:38:57 +0300

    added default sponsor

--- src/app/models/mock-sponsors.ts
+++ src/app/models/mock-sponsors.ts
@@ -2,10 +2,10 @@
 import {Sponsor} from "./sponsor";
 
 export const SPONSORS: Sponsor[] = [
-    {sponsorId: 1, type: 'diamond', url: 'http://drupalcamp.london/node/251', image: 'city_uol_logo.svg'},
-    {sponsorId: 2, type: 'diamond', url: 'http://drupalcamp.london/node/6', image: 'acquia_logo.svg'},
-    {sponsorId: 3, type: 'golden', url: 'http://drupalcamp.london/node/8', image: 'brightlemon_logo.svg'},
-    {sponsorId: 4, type: 'golden', url: 'http://drupalcamp.london/node/221', image: 'inviqa_logo.svg'},
-    {sponsorId: 5, type: 'golden', url: 'http://drupalcamp.london/node/9', image: 'cameron_wilding_logo.svg'},
-    {sponsorId: 6, type: 'golden', url: 'http://drupalcamp.london/node/311', image: 'capgemini_logo.svg'},
+    {sponsorId: 1, type: 'diamond', url: 'http://drupalcamp.london/node/251', image: 'sponsor/sp1.png'},
+    {sponsorId: 2, type: 'diamond', url: 'http://drupalcamp.london/node/6', image: 'sponsor/sp2.png'},
+    {sponsorId: 3, type: 'golden', url: 'http://drupalcamp.london/node/8', image: 'sponsor/sp3.png'},
+    {sponsorId: 4, type: 'golden', url: 'http://drupalcamp.london/node/221', image: 'sponsor/sp4.png'},
+    {sponsorId: 5, type: 'golden', url: 'http://drupalcamp.london/node/9', image: 'sponsor/sp1.png'},
+    {sponsorId: 6, type: 'golden', url: 'http://drupalcamp.london/node/311', image: 'sponsor/sp2.png'},
 ];
